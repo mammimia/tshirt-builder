458098ff073d0b32490ce84412a0b187ec4d746d
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-04-28 16:44:02 +0300

    Active tab logic implemented

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -16,6 +16,27 @@ import Tab from '../components/Tab';
 
 const Customizer = () => {
   const snap = useSnapshot(state);
+  const [file, setFile] = useState('');
+  const [prompt, setPrompt] = useState('');
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [activeEditorTab, setActiveEditorTab] = useState('');
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false
+  });
+
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case 'colorpicker':
+        return <ColorPicker />;
+      case 'filepicker':
+        return <FilePicker />;
+      case 'aipicker':
+        return <AIPicker />;
+      default:
+        return null;
+    }
+  };
 
   return (
     <AnimatePresence>
@@ -29,7 +50,11 @@ const Customizer = () => {
             <div className="flex items-center min-h-screen">
               <div className="editortabs-container tabs">
                 {EditorTabs.map((tab) => (
-                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => setActiveEditorTab(tab.name)}
+                  />
                 ))}
               </div>
             </div>
