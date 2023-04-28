efd1278855291facc656ebde192a67a4a1accca9
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-04-28 16:00:02 +0300

    Canvas and shirt initialized

--- client/src/canvas/Canvas.jsx
+++ client/src/canvas/Canvas.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Environment, Center } from '@react-three/drei';
 
-const Canvas = () => {
-  return <div>Canvas</div>;
+import Shirt from './Shirt';
+import Backdrop from './Backdrop';
+import CameraRig from './CameraRig';
+
+const CanvasModel = () => {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+
+      <Center>
+        <Shirt />
+      </Center>
+    </Canvas>
+  );
 };
 
-export default Canvas;
+export default CanvasModel;
